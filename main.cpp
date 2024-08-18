bfb5a7dff5ff8b78e0a4a209fead1c8d5e238975
mh607 <934505237@qq.com>
2024-08-18 10:53:56 +0000

    daydaycode 初始化

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
-#include "Soldier.h"
 #include <iostream>
 
 using namespace std;
 
-void Test()
-{
-    Soldier soldier("xusanduo", make_unique<Gun>("AK47", 0));
-    soldier.AddBulletToGun(10);
-    soldier.Fire();
-}
-
 int main(int argc, char** argv)
 {
     cout << "Hello World!" << endl;
-    Test();
     return 0;
 }
